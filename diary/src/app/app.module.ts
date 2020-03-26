fd6a629b6b98b34e79db92482b6aea6f9f92d4e0
victormongare1 <vicmongz254@gmail.com>
2020-03-26 11:51:51 +0300

    created diary component

--- diary/src/app/app.module.ts
+++ diary/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiaryComponent } from './diary/diary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiaryComponent
   ],
   imports: [
     BrowserModule,
